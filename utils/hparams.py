9fe2e74292a48cc81ba9cc34464af4600ee45486
sandycancode <sandeepsn@yahoo.com>
2021-02-10 14:49:10 +0100

    quick fix

--- utils/hparams.py
+++ utils/hparams.py
@@ -1,21 +1,4 @@
-'''
-hparams = {
-    'accumulate_grad_batches': 16,
-    'batch_size': 4,
-    'dev_limit': None,
-    'early_stop_patience': 3,
-    'embedding_dim': 768,
-    'fusion_output_size': 512,
-    'language_feature_dim': 768,
-    'lr': 5e-05,
-    'max_epochs': 10,
-    'n_gpu': 0,
-    'vision_feature_dim': 768
-    }
-'''
-
-hparams = {
-    
+hparams = { 
     # Optional hparams
     "embedding_dim": 768,
     "language_feature_dim": 768,
@@ -28,5 +11,5 @@ hparams = {
     "batch_size": 8,
     # allows us to "simulate" having larger batches 
     "accumulate_grad_batches": 16,
-    "early_stop_patience": 3,
+    "early_stop_patience": 3
 }
